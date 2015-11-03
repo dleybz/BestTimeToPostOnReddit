53c15b2d5530a519f043cf5d05f1d2c88fed11ba
theleybzon <dannyleybzon@gmail.com>
2015-11-03 15:38:13 -0800

    Updated SQL

--- BestTimeToPost.R
+++ BestTimeToPost.R
@@ -2,8 +2,6 @@
 list.of.packages <- c("bigrquery", "ggplot2", "methods", "dplyr", "grid")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
-
-#These lines open the libraries referenced in the following code
 library(bigrquery)
 library(ggplot2)
 library(methods)
@@ -14,28 +12,54 @@ library(grid)
 subreddit_desired <- "news" #INSERT SUBREDDIT YOU WISH TO ACCESS
 project_name <- "serene-boulder-108923" #INSERT PROJECT NAME
 
-sql <- "SELECT
-  DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5)) as day_as_int,
-  HOUR(SEC_TO_TIMESTAMP(created - 60*60*5)) as hour_as_int,
-  SUM(score) as total_score,
+sql <- "SELECT CASE
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=0 THEN '12AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=1 THEN '1AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=2 THEN '2AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=3 THEN '3AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=4 THEN '4AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=5 THEN '5AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=6 THEN '6AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=7 THEN '7AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=8 THEN '8AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=9 THEN '9AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=10 THEN '10AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=11 THEN '11AM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=12 THEN '12PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=13 THEN '1PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=14 THEN '2PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=15 THEN '3PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=16 THEN '4PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=17 THEN '5PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=18 THEN '6PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=19 THEN '7PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=20 THEN '8PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=21 THEN '9PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=22 THEN '10PM'
+WHEN HOUR(SEC_TO_TIMESTAMP(created - 60*60*5))=23 THEN '11PM'
+END as hour_as_char,
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=1 THEN 'Sunday'
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=2 THEN 'Monday'
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=3 THEN 'Tuesday'
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=4 THEN 'Wednesday'
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=5 THEN 'Thusday'
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=6 THEN 'Friday'
+WHEN DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5))=7 THEN 'Saturday'
+END as day_as_char,
+HOUR(SEC_TO_TIMESTAMP(created - 60*60*5)) as hour_as_int,
+DAYOFWEEK(SEC_TO_TIMESTAMP(created - 60*60*5)) as day_as_int,
+SUM(score) as total_score,
 FROM [fh-bigquery:reddit_posts.full_corpus_201509]
-WHERE subreddit=\"news\" /*insert subreddit of choice here*/
-GROUP BY hour_as_int, day_as_int
-ORDER BY day_as_int, hour_as_int"
+WHERE subreddit=subreddit_desired
+GROUP BY hour_as_int, day_as_int, day_as_char, hour_as_char
+ORDER BY day_as_int, hour_as_int, day_as_char, hour_as_char"
 
 #This line queries BigQuery and creates a new dataframe called "starting_table". The first time you do this you'll need to enter credentials; simply follow the instructions in the console
-starting_table <- tbl_df(query_exec(sql, project=project_name, max_pages=Inf))
-
-#These lines create dataframes which will be used to change the days of the week and hours of the day from their integer identifications to plain-text identifications
-day_as_char <- data_frame(day_as_int = 1:7, day_as_char = c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"))
-hour_as_char <- data_frame(hour_as_int = 0:23, hour_as_char = c(paste(c(12,1:11),"AM"), paste(c(12,1:11),"PM")))
-
-#This line creates a new dataframe composed of the three old dataframes we just made
-ending_table <- starting_table %>% left_join(day_as_char) %>% left_join(hour_as_char)
+table <- tbl_df(query_exec(sql, project=project_name, max_pages=Inf))
 
 #These lines level the variables; this is to make sure that our days of the week are in Sunday-Saturday order, instead of in alphabetical order
-ending_table$day_as_char <- factor(ending_table$day_as_char, level = rev(day_as_char$day_as_char))
-ending_table$hour_as_char <- factor(ending_table$hour_as_char, level = hour_as_char$hour_as_char)
+table$day_as_char <- factor(table$day_as_char, level = rev(day_as_char$day_as_char))
+table$hour_as_char <- factor(table$hour_as_char, level = hour_as_char$hour_as_char)
 
 #This line creates the title which will be generated in the image, which includes the best day and time to post
 the_title<-paste("The best time to post on /r/",subreddit_desired, "is\n", ending_table$day_as_char[ending_table$total_score == max(ending_table$total_score)], "at", ending_table$hour_as_char[ending_table$total_score == max(ending_table$total_score)])
